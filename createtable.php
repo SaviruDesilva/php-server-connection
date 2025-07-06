bd49266ec7173549f81b2509bfdfddd22860a799
SaviruDeSilva <savirurandika25@gmail.com>
2025-07-06 12:43:01 +0530

    Add files via upload

---
+++ createtable.php
@@ -0,0 +1,33 @@
+<?php
+
+include("conn.php");
+
+mysqli_select_db($conn,"jobseekers");
+
+//create tables
+
+$sql1="CREATE TABLE personal_info(id int AUTO_INCREMENT PRIMARY KEY,title varchar(10),name varchar(100),gender varchar(10),dateofbirth date)";
+
+
+$result1=mysqli_query($conn,$sql1);
+
+
+if(!$result1){
+
+	die("invalid query".mysqli_error());
+
+}
+
+
+else{
+
+
+	echo"table created successfully";
+
+
+}
+
+mysqli_close($conn);
+
+
+?>
